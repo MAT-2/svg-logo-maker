7bc59201b7b25918943ef3bda323d0f597300632
MAT-2 <mpcasey30@gmail.com>
2023-10-22 22:40:00 -0500

    Added inquirer prompt to ask questions to user to create SVG.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+const inquirer = require("inquirer");
+const fs = require("inquirer");
+
 (function () {
   console.log("Hello World");
 })();
+
+//List of questions user will be prompted to create SVG.
+inquirer.prompt([
+  {
+    type: "input",
+    message: "Please enter up to 3 characters total.",
+    name: "characters",
+  },
+  {
+    type: "input",
+    message:
+      "Please provide a color/hexadecimal of choice for your characters.",
+    name: "colorChars",
+  },
+  {
+    type: "list",
+    message: "Choose what shape you would like to use.",
+    choices: ["circle", "square", "triangle"],
+    name: "shape",
+  },
+  {
+    type: "input",
+    message:
+      "Please provide a color/hexadecimal of choice for the shape you chosed.",
+    name: "colorShape",
+  },
+]);
