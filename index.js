027a68c9580867b900d0d3551bc7ac3a399a98de
MAT-2 <mpcasey30@gmail.com>
2023-10-24 16:58:33 -0500

    Added comments to explain method creations for setting color, and created method for setting color for characters. Then used the .then method to take inquirer data, and created if statement that checks the shape chosen from inquirer and creating instances for shape variable to use setColor and setColorChars using the inquirer data. Also created fs method to writeFile and catch method if error occurs.

--- index.js
+++ index.js
@@ -4,28 +4,45 @@ const fs = require("inquirer");
 const { Circle, Triangle, Square } = require("./lib/shape.js");
 
 //List of questions user will be prompted to create SVG.
-inquirer.prompt([
-  {
-    type: "input",
-    message: "Please enter up to 3 characters total.",
-    name: "characters",
-  },
-  {
-    type: "input",
-    message:
-      "Please provide a color/hexadecimal of choice for your characters.",
-    name: "colorChars",
-  },
-  {
-    type: "list",
-    message: "Choose what shape you would like to use.",
-    choices: ["circle", "square", "triangle"],
-    name: "shape",
-  },
-  {
-    type: "input",
-    message:
-      "Please provide a color/hexadecimal of choice for the shape you chosed.",
-    name: "colorShape",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Please enter up to 3 characters total.",
+      name: "characters",
+    },
+    {
+      type: "input",
+      message:
+        "Please provide a color/hexadecimal of choice for your characters.",
+      name: "colorChars",
+    },
+    {
+      type: "list",
+      message: "Choose what shape you would like to use.",
+      choices: ["Circle", "Square", "Triangle"],
+      name: "shape",
+    },
+    {
+      type: "input",
+      message:
+        "Please provide a color/hexadecimal of choice for the shape you chosed.",
+      name: "colorShape",
+    },
+  ])
+  .then((inquirer) => {
+    let shape;
+    if (inquirer.shape === "Circle") {
+      shape = new Circle();
+      shape.setColor(inquirer.colorShape);
+      shape.setColorChars(inquirer.colorChars);
+    } else if (inquirer.shape === Triangle) {
+    } else {
+    }
+    fs.writeFile("shape.svg", shape, (err) => {
+      err ? console.log(err) : console.log("SVG generated!");
+    });
+  })
+  .catch((err) => {
+    console.log("ERROR: Please include the details specified.");
+  });
